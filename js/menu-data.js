25bac35316b2c099ef9ba308101eafbb313e4b29
Nina Dang <ninadangt@gmail.com>
2013-11-03 18:07:03 -0800

    no message

---
+++ js/menu-data.js
@@ -0,0 +1,11 @@
+/*
+File for creating a dynamic menu. Allows edits without altering the menu HTML file. 
+*/
+
+//Structure of menu 
+<script src="http://www.dawgpizza.com/orders/menu.js"></script>
+
+$(function(){
+	
+	
+});
